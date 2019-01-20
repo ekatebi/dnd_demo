343102554625ad02259db5ee57a1566fb9daf7ad
ekatebi-Yottaa <ekatebi@gmail.com>
2019-01-19 22:10:11 -0500

    add sticky items container

--- src/components/yoDraggables.js
+++ src/components/yoDraggables.js
@@ -20,14 +20,12 @@ export default class YoDraggables extends Component {
           <YoDraggable tag="DonutChart" id={id++} />
 
           <YoDraggable tag="Iframe" width="100%" height="100%" title="Grafana" id={id++}
-              src="https://j5dash.yottaa.com/d/o7AbP05mk/third-party-drill-down?orgId=1&refresh=5m&panelId=2&fullscreen&from=now-7d&to=now"
-          />
+              src="https://j5dash.yottaa.com/d/o7AbP05mk/third-party-drill-down?orgId=1&refresh=5m&panelId=2&fullscreen&from=now-7d&to=now" />
 
           <YoDraggable tag="HcDonutChart" id={id++} />
           <YoDraggable tag="HcLineChart" id={id++} />
           <YoDraggable tag="HcLineChart" id={id++} />
           <YoDraggable tag="HcLineChart" id={id++} />
-        
         </div>
     );
   }
